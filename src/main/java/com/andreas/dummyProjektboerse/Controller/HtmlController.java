5663662c2aaa520b94870a7d8e386b73ef3515ad
andreas <andreas.paulick1@gmail.com>
2019-01-22 15:15:35 +0100

    Add root HTML page to enter and view postings

--- src/main/java/com/andreas/dummyProjektboerse/Controller/HtmlController.java
+++ src/main/java/com/andreas/dummyProjektboerse/Controller/HtmlController.java
@@ -1,13 +1,33 @@
 package com.andreas.dummyProjektboerse.Controller;
 
+import com.andreas.dummyProjektboerse.Entity.Posts;
+import com.andreas.dummyProjektboerse.Repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HtmlController {
 
-    @GetMapping(path="/")
-    public String index() {
-        return "index";
+    @Autowired
+    private PostRepository postRepository;
+
+    @GetMapping("/")
+    public String postsForm(Model model) {
+        model.addAttribute("addPost", new Posts());
+        model.addAttribute("posts", postRepository.findAll());
+        return "posts";
+    }
+
+    @PostMapping("/")
+    public String postsSubmit(@ModelAttribute("addPost") Posts posts) {
+        posts.setTitle(posts.getTitle());
+        posts.setContent(posts.getContent());
+        posts.setStatus("publish");
+        postRepository.save(posts);
+        return "redirect:";
     }
 }
--- src/main/java/com/andreas/dummyProjektboerse/Controller/MainController.java
+++ src/main/java/com/andreas/dummyProjektboerse/Controller/MainController.java
@@ -25,7 +25,7 @@ public class MainController {
             , @RequestParam String content, @RequestParam String status) {
         // @ResponseBody means the returned String is the response, not a view name
         // @RequestParam means it is a parameter from the GET or POST request
-
+//
         Posts n = new Posts();
         n.setTitle(title);
         n.setContent(content);
