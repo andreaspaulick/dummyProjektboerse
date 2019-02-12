a14ad28f5aaf7355f80e65ec8605e6c0271844d7
andreas <andreas.paulick1@gmail.com>
2019-02-12 22:34:10 +0100

    remove unused imports

--- src/main/java/com/andreas/dummyProjektboerse/Controller/HtmlController.java
+++ src/main/java/com/andreas/dummyProjektboerse/Controller/HtmlController.java
@@ -5,15 +5,12 @@ import com.andreas.dummyProjektboerse.Entity.Posts;
 import com.andreas.dummyProjektboerse.Repository.PostRepository;
 import org.apache.http.auth.AuthenticationException;
 import org.apache.http.auth.UsernamePasswordCredentials;
-import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.auth.BasicScheme;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.keycloak.KeycloakPrincipal;
-import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -21,13 +18,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.Base64;
 
 @Controller
 public class HtmlController {
